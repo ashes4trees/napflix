f5e97ff1969d90a4cb3a5aa4f164674cbfe0b0a3
ashes4trees <stephsoni18@gmail.com>
2021-12-30 14:34:22 -0500

    complete project skeleton and initial index render

---
+++ frontend/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Welcome to Napflix!</h1>, root);
+})
