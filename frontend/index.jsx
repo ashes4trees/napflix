e0a98bfd63fc659ef3217a426e59eeb0b9098ecf
ashes4trees <stephsoni18@gmail.com>
2021-12-30 17:35:10 -0500

    add react app and root components

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to Napflix!</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 
     // delete later!!
     window.getState = store.getState;
