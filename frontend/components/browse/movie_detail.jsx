9ad46d89c760eac8fe7822cdfdeb86208c4fd24d
ashes4trees <stephsoni18@gmail.com>
2022-01-09 23:01:24 -0500

    add modal without styling

--- frontend/components/browse/movie_detail.jsx
+++ frontend/components/browse/movie_detail.jsx
@@ -4,7 +4,14 @@ import getStorage from "redux-persist/es/storage/getStorage";
 class MovieDetail extends React.Component {
     constructor(props) {
         super(props)
-    
+
+        this.state = {
+            sound: true,
+            showModal: false
+        }
+    this.autoplay = this.autoplay.bind(this);
+    this.soundOff = this.soundOff.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
     }
 
     movieGenres() {
@@ -15,29 +22,84 @@ class MovieDetail extends React.Component {
 
     autoplay(e) {
 
-        const video = e.currentTarget;
-        // debugger
-        video.classList.toggle('idle')
+        const video = e.currentTarget.children[1];
+        
+        this.state.sound ? video.volume = 1 : video.volume = 0
+        video.previousElementSibling.classList.toggle('invisible');
+        video.nextElementSibling.classList.toggle('invisible');
+        video.currentTime = 0;
+        video.classList.toggle('idle');
+        // ('.sound-off').classList.toggle('invisible');
         video.play()
     }
 
-    delayPlay() {
-        return setTimeout(() => this.autoplay, 1000)
+    soundOff() {
+        this.setState({sound: false});
+    }
+
+    toggleModal() {
+        const bool = this.state.showModal ?  false : true;
+        this.setState({showModal: bool});
     }
 
     render() {
         const tags = this.movieGenres();
         const display = tags.map(tag => <p key={tag.id}>{tag.genre}</p> )
-        
+        const modal = this.state.showModal ? (
+            <div className='modal'>
+                <img className='modal-thumbnail' src={this.props.movie.photoUrl} alt="" />
+                <div className='modal-vid-container'
+                    // onMouseOver={this.autoplay}
+                    // onMouseOut={this.autoplay}
+                >
+                    <p className='modal-title'>{this.props.movie.title}</p>
+                    <button className='top-play'>&#9658; Play</button>
+                    <button id='details-add-list'>+</button>
+                    <video
+                        className='modal-vid'
+                        src={window.movie}
+
+                    ></video>
+
+
+                    <img src={window.volume}
+                        className='modal-sound-off'
+                        onClick={this.soundOff} />
+                </div>
+
+                <div className='modal-details'>
+                    <p>{this.props.movie.summary}</p>
+                    <p className='modal-rating'>{this.props.movie.rating}</p>
+                    <div className='modal-tags' >
+                        {display}
+                    </div>
+
+                </div>
+            </div>
+        ) : null
         return (
+           
             <div className='list-item'>
                 <img className='thumbnail' src={this.props.movie.photoUrl} alt="" />
-                <video 
-                    className='thumbnail-vid idle' 
-                    src={window.movie}
-                    onMouseEnter={this.autoplay}
-                    onMouseLeave={this.autoplay}
+                <div className='details-vid-container'
+                    onMouseOver={this.autoplay}
+                    onMouseOut={this.autoplay}
+                >
+                    <p className='details-title invisible'>{this.props.movie.title}</p>
+                    <video
+                        className='thumbnail-vid idle'
+                        src={window.movie}
+                        
                     ></video>
+
+                    
+                    <img src={window.volume} 
+                        className='sound-off invisible'
+                        onClick={this.soundOff} />
+                </div>
+                
+                {/* <img src={window.volume} className='sound-off invisible' /> */}
+                
                 
                 <div className='movie-details hidden'>
                     <div className='details-btns'>
@@ -45,7 +107,7 @@ class MovieDetail extends React.Component {
                             <button id="details-play">&#9658;</button>
                             <button id='details-add-list'>+</button>
                         </div>
-                        <button id='details-info-btn'>
+                        <button onClick={this.toggleModal} id='details-info-btn'>
                             <p>&#8964;</p>
                         </button>
                     
@@ -57,6 +119,7 @@ class MovieDetail extends React.Component {
                     </div>
                 
                 </div>
+                {modal}
             </div>
         
         )
